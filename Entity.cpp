ad0a19bb2ac6643143df51c3e17d73b4f902ddf4
izz faris <izzfaris14@gmail.com>
2026-05-31 21:02:36 +0800

    added entity.cpp

---
+++ Entity.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "Entity.h"
+
+using namespace std;
+
+Entity::Entity(string id, string dest, time_t time) : id(id), dest(dest), time(time) {}
+
+void Entity::displayInfo() const {
+	cout << "ID: " << id << endl;
+	cout << "Destination: " << dest << endl;
+	cout << "Time: " << ctime(&time);
+}
+
+string Entity::getId() const {
+	return id;
+}
+
+string Entity::getDest() const {
+	return dest;
+}
+
+time_t Entity::getTime() const {
+	return time;
+}
+
