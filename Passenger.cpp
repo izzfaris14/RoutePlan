493ac28a8f021c0c4394b9c98dc13c6d19566043
izz faris <izzfaris14@gmail.com>
2026-06-15 00:30:43 +0800

    added getDeets into .cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -5,3 +5,6 @@ using namespace std;
 
 Passenger::Passenger(string id, string dest, string timeStr) : Entity(id, dest, timeStr) {
 }
+
+string Passenger::getDeets() const {
+	return "Passenger ID: " + getId() + ", Dest " + getDest() + ", Time:" + getTimeStr();
--- Shuttle.cpp
+++ Shuttle.cpp
@@ -1,6 +1,11 @@
 #include"Shuttle.h"
 #include<iostream>
 
+
 using namespace std;
 
 Shuttle::Shuttle(string id, string dest, string timeStr) : Entity(id, dest, timeStr) {}
+
+string Shuttle::getDeets() const {
+	return "Shuttle ID: " + getId() + ", Dest " + getDest() + ", Time:" + getTimeStr();
+}
