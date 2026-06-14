88fa16213b2c142d341d5492eaa3626bc2b3d371
izz faris <izzfaris14@gmail.com>
2026-06-15 00:49:46 +0800

    -updated lists

--- Lists.cpp
+++ Lists.cpp
@@ -14,3 +14,33 @@ ShuttleList::ShuttleList(vector<Shuttle> loadedData) {
 Shuttle ShuttleList::getShuttle(int index) const {
 	return shuttleDB[index]; 
 }
+
+void ShuttleList::editShuttle(string id, Shuttle updated) {
+	for (int i = 0; i < shuttleDB.size(); i++) {
+		if (shuttleDB[i].getId() == id) {
+			shuttleDB[i] = updated;
+			break;
+		}
+	}
+}
+
+PassList::PassList(vector<Passenger> loadedData) {
+	passDB = loadedData;
+}
+
+vector<Passenger> PassList::getPassengers() const {
+	return passDB;
+}
+
+Passenger PassList::getPassenger(int index) const {
+	return passDB[index];
+}
+
+void PassList::editPassenger(string id, Passenger updated) {
+	for (int i = 0; i < passDB.size(); i++) {
+		if (passDB[i].getId() == id) {
+			passDB[i] = updated;
+			break;
+		}
+	}
+}
