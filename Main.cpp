4b36801b50182fa78312e2476e7832a0937d94be
izz faris <izzfaris14@gmail.com>
2026-06-17 22:48:28 +0800

    - added the new functions in the UI. adding, deleting and editing are separate functions

--- Main.cpp
+++ Main.cpp
@@ -19,16 +19,19 @@ int main() {
     RoutePlanner planner(sList, pList);
 
     int choice = 0;
-    while (choice != 6) {
+    // Updated the loop condition to 8 since we added two new options
+    while (choice != 8) {
         cout << "\n==========================================" << endl;
         cout << "  DRIVERLESS PUBLIC TRANSPORT SYSTEM  " << endl;
         cout << "==========================================" << endl;
         cout << "1. Generate Route Matches" << endl;
         cout << "2. Display Detailed Schedule" << endl;
         cout << "3. Display Unmatched Shuttles & Passengers" << endl;
-        cout << "4. Save Schedule to File" << endl;
-        cout << "5. Edit Parameters in RAM" << endl;
-        cout << "6. Exit System" << endl;
+        cout << "4. Add New Entity" << endl;       // NEW
+        cout << "5. Delete Entity" << endl;        // NEW
+        cout << "6. Edit Parameters in RAM" << endl;
+        cout << "7. Save Schedule to File" << endl;
+        cout << "8. Exit System" << endl;
         cout << "Enter your choice: ";
 
         cin >> choice;
@@ -42,10 +45,34 @@ int main() {
         else if (choice == 3) {
             planner.displayUnmatched();
         }
+        // --- NEW ADD FUNCTIONALITY ---
         else if (choice == 4) {
-            planner.writeFile("final_schedule.txt");
+            int type;
+            string id, dest, time;
+            cout << "\nAdd (1) Shuttle or (2) Passenger? ";
+            cin >> type;
+            cout << "Enter ID: "; cin >> id;
+            cout << "Enter Destination: "; cin >> dest;
+            cout << "Enter Time (e.g., 8:00am): "; cin >> time;
+
+            if (type == 1) planner.addShuttle(id, dest, time);
+            else if (type == 2) planner.addPassenger(id, dest, time);
+            else cout << "Invalid choice.\n";
         }
+        // --- NEW DELETE FUNCTIONALITY ---
         else if (choice == 5) {
+            int type;
+            string id;
+            cout << "\nDelete (1) Shuttle or (2) Passenger? ";
+            cin >> type;
+            cout << "Enter ID to delete: "; cin >> id;
+
+            if (type == 1) planner.deleteShuttle(id);
+            else if (type == 2) planner.deletePassenger(id);
+            else cout << "Invalid choice.\n";
+        }
+        // --- SHIFTED EDIT FUNCTIONALITY ---
+        else if (choice == 6) {
             int editChoice;
             string id, dest, time;
             cout << "\n1. Edit Shuttle\n2. Edit Passenger\nChoice: ";
@@ -65,8 +92,13 @@ int main() {
                 cout << "Invalid edit choice." << endl;
             }
         }
-        else if (choice == 6) {
-            cout << "Exiting system. Have a good day!" << endl;
+        // --- SHIFTED SAVE FUNCTIONALITY ---
+        else if (choice == 7) {
+            planner.writeFile("final_schedule.txt");
+        }
+        // --- SHIFTED EXIT FUNCTIONALITY ---
+        else if (choice == 8) {
+            cout << "Exiting system..." << endl;
         }
         else {
             cout << "Invalid choice. Please try again." << endl;
