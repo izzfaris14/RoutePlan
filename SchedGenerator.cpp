bb866e19f46f2f7de3541f51f5f86067fe86fcfb
izz faris <izzfaris14@gmail.com>
2026-07-03 20:54:09 +0800

    -updated schedgenerator

--- SchedGenerator.cpp
+++ SchedGenerator.cpp
@@ -1,18 +1,19 @@
 #include "SchedGenerator.h"
+#include "Route.h"
 
 void SchedGenerator::generateMatches(SchedRepo& repo) {
 	const auto& passengers = repo.getPassengers();
 	const auto& shuttles = repo.getShuttles();
 
-	for (const& p : passengers) {
-		if (p->getIsAssinged()) continue; //skip if passenger already matched
+	for (const auto& p : passengers) {
+		if (p->getIsAssigned()) continue; //skip if passenger already matched
 
 		for (const auto& s : shuttles) {
 			if (s->getIsAssigned()) continue; //skip if shuttle already matched
 
-			if (p->getDest == s->getDest() && p->getTimeStr() == s->getTimeStr()) {
+			if (p->getDest() == s->getDest() && p->getTimeStr() == s->getTimeStr()) {
 
-				repo.addRoute(Route(s.get(), p.get());
+				repo.addRoute(Route(s.get(), p.get()));
 
 				p->setAssigned(true);
 				s->setAssigned(true);
