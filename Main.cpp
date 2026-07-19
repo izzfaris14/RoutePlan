e4c2eac56bc45bc4262fcfcb593d9e501cb8cda4
izz faris <izzfaris14@gmail.com>
2026-07-19 23:15:25 +0800

    -updated main

--- Main.cpp
+++ Main.cpp
@@ -7,23 +7,19 @@
 using namespace std;
 
 int main() {
-    // 1. Initialize our decoupled architecture
-    SchedRepo repo;
-    SchedGenerator generator;
-    FileParser parser;
-    UIControl* ui = UIControl::getInstance();
+	SchedRepo repo;
+	SchedGenerator generator;
+	FileParser parser;
+	UIControl* ui UIControl::getInstance();
 
-    cout << "System initializing...\n";
+	cout << "system initializing\n";
 
-    // 2. Load the data using FileParser
-    parser.loadShuttles("shuttle.txt", repo);
-    parser.loadPassengers("passenger.txt", repo);
+	parser.loadShuttles("shuttles.txt", repo);
+	parser.loadPassengers("passenger.txt", repo);
 
-    // 3. Generate the matches! (This is what you were missing)
-    generator.generateMatches(repo);
+	generator.generateMatches(repo);
 
-    // 4. Display the results
-    ui->displayMatches(repo);
+	ui->displayMatches(repo);
 
-    return 0;
+	return 0;
 }
