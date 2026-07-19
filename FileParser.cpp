4d9fecf337940d432c80b4a7c515dec5acfde73b
izz faris <izzfaris14@gmail.com>
2026-07-19 17:59:28 +0800

    -fileparser

--- FileParser.cpp
+++ FileParser.cpp
@@ -14,31 +14,35 @@ void FileParser::loadShuttles(const std::string& filename, SchedRepo& repo) {
 	}
 	std::string line;
 	while (std::getline(file, line)) {
-		if (line.empty()) continue; // Skip empty lines
+		if (line.empty()) continue;
 
 		std::stringstream ss(line);
-		std::string id, dest, timeStr, CapacityStr;
+		std::string id, dest, timeStr, capacityStr;
 
 		std::getline(ss, id, ',');
 		std::getline(ss, dest, ',');
 		std::getline(ss, timeStr, ',');
-		std::getline(ss, CapacityStr, ',');
+		std::getline(ss, capacityStr, ',');
 
-		if (!CapacityStr.empty() && CapacityStr.back() == '\r') {
-			CapacityStr.pop_back();
-	}
+		// Remove invisible carriage returns (\r) from Windows text files
+		if (!capacityStr.empty() && capacityStr.back() == '\r') {
+			capacityStr.pop_back();
+		}
+
+		// Part 2 Spec: Map "Small", "Family", "Premium" to actual integer capacities
 		int capacity = 0;
-		if (CapacityStr == "Small") capacity = 4;
-		else if (CapacityStr == "Family") capacity = 7;
-		else if (CapacityStr == "Premium") capacity = 15;
-		else if (!CapacityStr.empty()) {
-			try { capacity = std::stoi(CapacityStr); }
-			catch (...) { capacity = 4; }
+		if (capacityStr == "Small") capacity = 4;
+		else if (capacityStr == "Family") capacity = 7;
+		else if (capacityStr == "Premium") capacity = 15;
+		else if (!capacityStr.empty()) {
+			try { capacity = std::stoi(capacityStr); }
+			catch (...) { capacity = 4; } // Default fallback
 		}
 		else {
-			capacity = 4;
+			capacity = 4; // Default if column is completely missing
 		}
-		repo.addShuttle(std::make_unique<Shuttle>)(id, dest, timeStr, capacity));
+
+		repo.addShuttle(std::make_unique<Shuttle>(id, dest, timeStr, capacity));
 	}
 	file.close();
 }
@@ -49,25 +53,20 @@ void FileParser::loadPassengers(const std::string& filename, SchedRepo& repo) {
 		std::cerr << "Error opening file: " << filename << std::endl;
 		return;
 	}
-
 	std::string line;
 	while (std::getline(file, line)) {
-		if (line.empty()) continue; // Skip empty lines
+		if (line.empty()) continue;
 
 		std::stringstream ss(line);
-		std::string id, name, dest, timeStr, partySize;
+		std::string id, dest, timeStr, sizeStr;
 
 		std::getline(ss, id, ',');
 		std::getline(ss, dest, ',');
 		std::getline(ss, timeStr, ',');
-		std::getline(ss, partySize, ',');
+		std::getline(ss, sizeStr, ',');
 
-		// Default to 1 if partySize is empty
-
-		int size = partySize.empty() ? 1 : std::stoi(partySize); 
-
-		repo.addPassenger(std::make_unique<Passenger>(id, dest, timeStr,size));
+		int partySize = sizeStr.empty() ? 1 : std::stoi(sizeStr);
+		repo.addPassenger(std::make_unique<Passenger>(id, dest, timeStr, partySize));
 	}
 	file.close();
 }
-
