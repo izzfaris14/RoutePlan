dca4f1bcedec561a7340ab6f3a768366196f3100
izz faris <izzfaris14@gmail.com>
2026-07-19 23:08:32 +0800

    -updating UI class

--- UIControl.cpp
+++ UIControl.cpp
@@ -6,24 +6,25 @@
 UIControl* UIControl::instance = nullptr;
 
 UIControl* UIControl::getInstance() {
-	if (instance = nullptr) {
+	if (instance == nullptr) {
 		instance = new UIControl();
 	}
 	return instance;
 }
+
 void UIControl::displayMatches(const SchedRepo& repo) {
-	std::cout << "\n=== GENERATED ROUTES ===\n" << std::endl;
+	std::cout << "\n===GENERATED ROUTES ===\n" << std::endl;
 
 	const auto& routes = repo.getRoutes();
 
 	if (routes.empty()) {
-		std::cout << "No matches found." << std::endl;
+		std::cout << "no mathces found" << std::endl;
 	}
 	else {
 		for (const auto& r : routes) {
-			std::cout << "-----------------------------------" << std::endl;
+			std::cout << "----" << std::endl;
 			std::cout << r.getRouteString() << std::endl;
 		}
 	}
-	std::cout << "===================================\n" << std::endl;
+	std::cout << "====\n" << std::endl;
 }
--- UIControl.h
+++ UIControl.h
@@ -1,13 +1,10 @@
 #pragma once
 #include "SchedRepo.h"
-#include <iostream>
 
 class UIControl {
 	static UIControl* instance;
 	UIControl() {}
-
 public:
 	static UIControl* getInstance();
-
 	void displayMatches(const SchedRepo& repo);
 };
