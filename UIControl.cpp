7161536b3309457738285e143a007d96c4101f29
izz faris <izzfaris14@gmail.com>
2026-07-19 17:15:40 +0800

    UI control

--- UIControl.cpp
+++ UIControl.cpp
@@ -1,28 +1,16 @@
-#include "UIControl.h"
-#include<iostream>
-
-
-//initialize the static instance pointer to null
-UIControl* UIControl::instance = nullptr; 
-
-UIControl* UIControl::getInstance() {
-	if (instance == nullptr) {
-		instance = new UIControl();
-	}
-	return instance;
-}
-
 void UIControl::displayMatches(const SchedRepo& repo) {
-	cout << "\n=== GENERATED ROUTES ===\n" << endl;
+	std::cout << "\n=== GENERATED ROUTES ===\n" << std::endl;
 
 	const auto& routes = repo.getRoutes();
 
 	if (routes.empty()) {
-		cout << "no matches found" << endl;
-	} else{
+		std::cout << "No matches found." << std::endl;
+	}
+	else {
 		for (const auto& r : routes) {
-			cout << "matched established" << endl;
+			std::cout << "-----------------------------------" << std::endl;
+			std::cout << r.getRouteString() << std::endl;
 		}
 	}
-	cout << "=================\n" << endl;
+	std::cout << "===================================\n" << std::endl;
 }
